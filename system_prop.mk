c943e2a04ad4a0450e7fad13b137baee4bf216b5
matteo0026 <marmotta200026@gmail.com>
2022-01-12 14:42:37 +0100

    matissevewifi: commonize volume
    Change-Id: If37851da816d9c0168e8e00467dc125aa886589b

--- system_prop.mk
+++ system_prop.mk
@@ -14,8 +14,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Screen
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.sf.lcd_density=160
-
-# Volume
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.vc_call_vol_steps=15 \
-    ro.config.media_vol_steps=30
